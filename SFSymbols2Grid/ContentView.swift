87db20e69d5a7b38cb0ce610c88f0e5ee73103ae
Mohammed Abunada <m7606225@gmail.com>
2020-06-27 01:02:58 +0200

    Initial Commit

---
+++ SFSymbols2Grid/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SFSymbols2Grid
+//
+//  Created by Mohammed Abunada on 2020-06-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SFSymbols2Grid/SFSymbols2GridApp.swift
@@ -0,0 +1,17 @@
+//
+//  SFSymbols2GridApp.swift
+//  SFSymbols2Grid
+//
+//  Created by Mohammed Abunada on 2020-06-27.
+//
+
+import SwiftUI
+
+@main
+struct SFSymbols2GridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

b8274515447e909613c185d095d01654ef2c6d99
Mohammed Abunada <m7606225@gmail.com>
2020-06-27 01:46:43 +0200

    SFSymbols2Grid
    New SF Symbols 2 Grid in SwiftUI 2 20200627

--- SFSymbols2Grid/ContentView.swift
+++ SFSymbols2Grid/ContentView.swift
@@ -1,20 +1,64 @@
-//
+// https://github.com/m760622/SFSymbols2Grid
 //  ContentView.swift
 //  SFSymbols2Grid
-//
+// m7606225@gmail.com
 //  Created by Mohammed Abunada on 2020-06-27.
-//
+// https://github.com/m760622
 
 import SwiftUI
 
+private var colors: [Color] = [.red, .blue, .secondary, .gray, .green, .yellow, .orange, .pink, .purple]
+
 struct ContentView: View {
+    let columns = [ GridItem(.adaptive(minimum: 50)) ]
+
     var body: some View {
-        Text("Hello, world!").padding()
-    }
-}
+        NavigationView{
+            VStack {
+                ScrollView {
+                    LazyVGrid(columns: columns, spacing: 10) {
+                        ForEach(sfSymbols2, id: \.self){item in
+                            NavigationLink(destination: SFSymbolsImage(imageName: item, detailView: true) )
+                            {
+                                SFSymbolsImage(imageName: item, detailView: false)
+                            }//NavigationLink
+                        }
+                    }//LazyVGrid
+                }//ScrollView
+                .padding(.all, 10)
+            }//VStack
+            .navigationBarTitle(Text("New SF Symbols 2 Grid in SwiftUI 2") , displayMode: .inline)
+        }//NavigationView
+    }//body
+}//ContentView
+
+struct SFSymbolsImage: View {
+    var imageName: String
+    var detailView: Bool
+    var body: some View {
+        VStack {
+            if detailView{
+                Text(imageName)
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(colors.randomElement())
+                    .padding(.all, 10 )
+            }
+            Image(systemName: imageName)
+                .resizable(capInsets: EdgeInsets(), resizingMode: .stretch)
+                .frame(width: detailView ? 300 : 50, height: detailView ? 300 : 50, alignment: .center)
+                .foregroundColor(colors.randomElement())
+                .padding(.all, 10)
+        }//HStack
+    }//body
+}//SFSymbolsImage
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+            ContentView()
+                .preferredColorScheme(.dark)
+        }
     }
 }
--- SFSymbols2Grid/SFSymbols2GridApp.swift
+++ SFSymbols2Grid/SFSymbols2GridApp.swift
@@ -1,9 +1,9 @@
-//
+// https://github.com/m760622/SFSymbols2Grid
 //  SFSymbols2GridApp.swift
 //  SFSymbols2Grid
-//
+// m7606225@gmail.com
 //  Created by Mohammed Abunada on 2020-06-27.
-//
+// https://github.com/m760622
 
 import SwiftUI
 
